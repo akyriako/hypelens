8f007befb2e5d5dcd1292169cb626db1e261ebe0
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2020-12-16 09:46:33 +0100

    First commit of Hypelens full template

---
+++ Hypelens/src/js/demo/form-elements.demo.js
@@ -0,0 +1,17 @@
+/*
+Template Name: ASPHypelens - Responsive Bootstrap 4 Admin Template
+Version: 1.1.0
+Author: Sean Ngu
+Website: http://www.seantheme.com/asp-Hypelens/
+*/
+
+var handleInitBsFileInput = function() {
+	bsCustomFileInput.init();
+};
+
+
+/* Controller
+------------------------------------------------ */
+$(document).ready(function() {
+	handleInitBsFileInput();
+});
---
+++ Hypelens/wwwroot/js/demo/page-coming-soon.demo.js
@@ -0,0 +1,19 @@
+/*
+Template Name: ASPHypelens - Responsive Bootstrap 4 Admin Template
+Version: 1.1.0
+Author: Sean Ngu
+Website: http://www.seantheme.com/asp-Hypelens/
+*/
+
+var handleRenderCountdownTimer = function() {
+	var newYear = new Date();
+	newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1);
+	$('#timer').countdown({until: newYear});
+};
+
+
+/* Controller
+------------------------------------------------ */
+$(document).ready(function() {
+	handleRenderCountdownTimer();
+});
