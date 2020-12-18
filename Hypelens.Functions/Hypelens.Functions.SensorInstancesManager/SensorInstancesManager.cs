ecdbafe31fb7a8410871609b85c1af11ae369b56
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2020-12-18 10:29:53 +0100

    Added SensorsInstancesManager Function

---
+++ Hypelens.Functions/Hypelens.Functions.SensorInstancesManager/SensorInstancesManager.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Hypelens.Functions.SensorInstancesManager
+{
+    public static class SensorInstancesManager
+    {
+        [FunctionName("SensorPipeline")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("SensorInstancesManager_Hello", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("SensorInstancesManager_Hello", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("SensorInstancesManager_Hello", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [FunctionName("SensorInstancesManager_Hello")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+            return $"Hello {name}!";
+        }
+
+        [FunctionName("StartSensorInstance")]
+        public static async Task StartSensor(
+            [ServiceBusTrigger("start-sensors-queue", Connection = "AzureServiceBus")] string queueItem,
+            [DurableClient] IDurableOrchestrationClient client,
+            ILogger log)
+        {
+            string instanceId = await client.StartNewAsync("SensorPipeline", null);
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+        }
+
+        [FunctionName("TerminateSensorInstance")]
+        public static void TerminateSensor(
+            [ServiceBusTrigger("terminate-sensors-queue", Connection = "AzureServiceBus")] string queueItem,
+            [DurableClient] IDurableOrchestrationClient client,
+            ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {queueItem}");
+        }
+
+        [FunctionName("QuerySensor")]
+        public static async Task<IActionResult> QuerySensor(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "Sensors/Status/{instanceId}")] HttpRequestMessage httpRequestMessage,
+            [DurableClient] IDurableOrchestrationClient client,
+            string instanceId,
+            ILogger log)
+        {
+            if (string.IsNullOrEmpty(instanceId))
+            {
+                return new BadRequestResult();
+            }
+            else
+            {
+                DurableOrchestrationStatus orchestrationStatus = await client.GetStatusAsync(instanceId);
+
+                if (orchestrationStatus != null)
+                {
+                    return new OkObjectResult(orchestrationStatus.RuntimeStatus.ToString());
+                }
+                else
+                {
+                    return new NotFoundResult();
+                }
+            }
+        }
+
+        //[FunctionName("SensorInstancesManager_HttpStart")]
+        //public static async Task<HttpResponseMessage> HttpStart(
+        //    [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "Sensors/Start")] HttpRequestMessage req,
+        //    [DurableClient] IDurableOrchestrationClient starter,
+        //    ILogger log)
+        //{
+        //    // Function input comes from the request content.
+        //    string instanceId = await starter.StartNewAsync("SensorInstancesManager", null);
+
+        //    log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+        //    return starter.CreateCheckStatusResponse(req, instanceId);
+        //}
+    }
+}
